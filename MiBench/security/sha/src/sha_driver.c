c537a08cd3124650f964c8d09bb583ec997fc1b0
Sangeeta Kakati <sangeeta.kakati19@gmail.com>
2023-07-05 12:24:59 +0200

    Add files via upload

--- MiBench/security/sha/src/sha_driver.c
+++ MiBench/security/sha/src/sha_driver.c
@@ -10,22 +10,40 @@ int main(int argc, char **argv)
 {
     FILE *fin;
     SHA_INFO sha_info;
+    clock_t start, end;
+    double cpu_time_used = 0.0;
 
-    if (argc < 2) {
-	fin = stdin;
-	sha_stream(&sha_info, fin);
-	sha_print(&sha_info);
-    } else {
-	while (--argc) {
-	    fin = fopen(*(++argv), "rb");
-	    if (fin == NULL) {
-		printf("error opening %s for reading\n", *argv);
-	    } else {
-		sha_stream(&sha_info, fin);
-		sha_print(&sha_info);
-		fclose(fin);
-	    }
-	}
+    if (argc < 2)
+    {
+        fin = stdin;
+
+        start = clock();
+        sha_stream(&sha_info, fin);
+        end = clock();
+        sha_print(&sha_info);
     }
-    return(0);
+    else
+    {
+        while (--argc)
+        {
+            fin = fopen(*(++argv), "rb");
+            if (fin == NULL)
+            {
+                printf("error opening %s for reading\n", *argv);
+            }
+            else
+            {
+                start = clock();
+                sha_stream(&sha_info, fin);
+                end = clock();
+                sha_print(&sha_info);
+                fclose(fin);
+            }
+        }
+    }
+
+    cpu_time_used += ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("Time taken: %.2f seconds\n", cpu_time_used);
+
+    return 0;
 }
