ec2c080e581dbb2783d00683c27bdd112f7af706
Sangeeta Kakati <sangeeta.kakati19@gmail.com>
2023-07-05 12:27:18 +0200

    Add files via upload

--- MiBench/telecomm/adpcm/rawcaudio/src/rawcaudio.c
+++ MiBench/telecomm/adpcm/rawcaudio/src/rawcaudio.c
@@ -2,28 +2,38 @@
 
 #include "adpcm.h"
 #include <stdio.h>
+#include <time.h>
 
 struct adpcm_state state;
 
 #define NSAMPLES 1000
 
-char	abuf[NSAMPLES/2];
-short	sbuf[NSAMPLES];
+char    abuf[NSAMPLES/2];
+short   sbuf[NSAMPLES];
 
-main() {
+int main() {
     int n;
+    clock_t start, end;
+
+    start = clock();
 
     while(1) {
-	n = read(0, sbuf, NSAMPLES*2);
-	if ( n < 0 ) {
-	    perror("input file");
-	    exit(1);
-	}
-	if ( n == 0 ) break;
-	adpcm_coder(sbuf, abuf, n/2, &state);
-	write(1, abuf, n/4);
+        n = read(0, sbuf, NSAMPLES*2);
+        if ( n < 0 ) {
+            perror("input file");
+            exit(1);
+        }
+        if ( n == 0 ) break;
+        adpcm_coder(sbuf, abuf, n/2, &state);
+        write(1, abuf, n/4);
     }
+    end = clock();
+
     fprintf(stderr, "Final valprev=%d, index=%d\n",
-	    state.valprev, state.index);
+            state.valprev, state.index);
+
+    double cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("Time taken: %.2f seconds\n", cpu_time_used);
+
     exit(0);
 }
