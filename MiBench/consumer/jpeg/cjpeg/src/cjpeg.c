75310cba5efe935976d3ef3fa4e8e7053f19216a
Sangeeta Kakati <sangeeta.kakati19@gmail.com>
2023-07-05 12:21:33 +0200

    Add files via upload

--- MiBench/consumer/jpeg/cjpeg/src/cjpeg.c
+++ MiBench/consumer/jpeg/cjpeg/src/cjpeg.c
@@ -22,7 +22,7 @@
  *	cjpeg [options]  -outfile outputfile  inputfile
  * works regardless of which command line style is used.
  */
-
+#include <time.h>
 #include "cdjpeg.h"		/* Common decls for cjpeg/djpeg applications */
 #include "jversion.h"		/* for version message */
 
@@ -457,9 +457,7 @@ parse_switches (j_compress_ptr cinfo, int argc, char **argv,
  * The main program.
  */
 
-int
-main (int argc, char **argv)
-{
+int main(int argc, char **argv) {
   struct jpeg_compress_struct cinfo;
   struct jpeg_error_mgr jerr;
 #ifdef PROGRESS_REPORT
@@ -471,6 +469,8 @@ main (int argc, char **argv)
   FILE * output_file;
   JDIMENSION num_scanlines;
 
+  clock_t start, end;
+  start = clock();  
   /* On Mac, fetch a command line. */
 #ifdef USE_CCOMMAND
   argc = ccommand(&argv);
@@ -599,6 +599,11 @@ main (int argc, char **argv)
 #ifdef PROGRESS_REPORT
   end_progress_monitor((j_common_ptr) &cinfo);
 #endif
+
+  end = clock();  // End timestamp
+
+  double cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+  printf("Time taken: %.2f seconds\n", cpu_time_used);
 
   /* All done. */
   exit(jerr.num_warnings ? EXIT_WARNING : EXIT_SUCCESS);
